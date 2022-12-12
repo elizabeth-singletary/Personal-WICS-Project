9242a1a47f0d516b6ccf99e46bdf8e96a8ad4448
elizabeth-singletary <103393032+elizabeth-singletary@users.noreply.github.com>
2022-12-11 23:12:14 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php include 'header.php' ?>
+
+<section class=" text-center">
+  <div class="d-flex container">
+    <!-- Text Plus Buttons -->
+      <div class="col-flex"> 
+      <div>
+        <div class="title">
+          
+          Women In Computer Science
+        </div>
+        <div class="container-fluid pb-3">
+          <p class="text-center">Women in Computer Science at UNC provides a community for women in the field of computer science and additional fields involving programming.  WICS is primarily concerned with providing opportunities to its club members via networking with partnered organizations and companies. We hold multiple workshops and seminars geared towards breaking into and navigating the tech world. </p>
+        </div>
+       </div>
+       <div class="justify-content-center">
+        <a type="button" class="btn btn-primary m-2" href="https://github.com/uncwics" target="_blank"><i class="bi bi-github"></i> Github</a>
+        <a type="button" class="btn btn-primary m-2" href="https://www.instagram.com/uncwics/" target="_blank"><i class="bi bi-instagram"></i> Instagram</a>
+        <?php
+        if (isset($_SESSION['firstname'])){
+          echo '<a type="button" class="btn btn-primary m-2" href="logout.php"><i class="bi bi-box-arrow-in-right"></i> Logout</a>';
+        } else {
+          echo '<a type="button" class="btn btn-primary m-2" href="loginpage.php"><i class="bi bi-box-arrow-in-right"></i> Login</a>';
+        }
+        
+        ?>
+      </div>
+      </div>
+      <!-- Image -->
+       
+  </div>
+</section>
+<!-- End of Hero Section -->
+   
+<!-- Join Us -->
+<section>
+  <div class="container">
+    <div class="row d-flex justify-content-center m-2 mt-5">
+      <!-- Service Item 1 -->
+      <div class="container col-lg-5 d-block service-item" style="height: 200px ;">
+        <div class="d-flex">
+          <img src="oldwell.svg" alt="" style="height: 55px; width:auto;" class="icon"></img>
+          <h3 class="service-item-title">Heel Life</h3>
+        </div>
+        <div class="container pt-3 service-item-text"><p>To be an official member of WiCS please join us on Heel Life!</p></div>
+      </div>
+      <!-- Service Item 2 -->
+      <div class="container col-lg-5 d-block service-item" style="height: 200px ;">
+        <div class="d-flex">
+          <i class="bi bi-calendar-event me-3" style="font-size: 45px;"></i>
+          <h3 class="service-item-title">Events</h3>
+        </div>
+        <div class="container service-item-text"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora exercitationem aperiam unde ab magnam ullam cum facilis explicabo eius culpa?</p></div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- End of Join Us -->
+
+
+<!-- Projects -->
+<section>
+  <div class="container justify-content-center d-block">
+    <div class="m-5"><h2>Our Projects</h2></div>
+    <div class="row d-flex justify-content-center">
+      <div class="col-lg-3 m-2"><img src="logo.png" alt="" class="img-fluid projectimg"></div>
+      <div class="col-lg-3 m-2"><img src="question.png" alt="" class="img-fluid projectimg"></div>
+      <div class="col-lg-3 m-2"><img src="question.png" alt="" class="img-fluid projectimg"></div>
+    </div>
+  </div>
+</section>
+<!-- End of Projects -->
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+      crossorigin="anonymous"
+    ></script>
+    <script src="index.js"></script>
+  </body>
+</html>
+
